1d2e021889eb3c15a8626e6d95fbebac30954609
Milashevskyi Bohdan <milasevskijbogdan@gmail.com>
2022-05-11 20:21:51 +0300

    added structures

--- To-do_List/To-do_List.cpp
+++ To-do_List/To-do_List.cpp
@@ -1,7 +1,35 @@
 #include "Header.h"
 
+struct TodoItem
+{
+	int id;
+	string name;
+	string description;
+	int day;
+	int month;
+	int year;
+	string deadline;
+
+};
+
+struct TodoList
+{
+	int size = 5;
+
+	int id;
+	string name;
+	TodoItem* todoitems = new TodoItem[size];
+
+	void addNewItem(TodoItem item);
+	void deleteItem(int id);
+	void updateItem(TodoItem item);
+	void clean();
+
+};
+
+
 int main()
 {
-
+	
 }
 
