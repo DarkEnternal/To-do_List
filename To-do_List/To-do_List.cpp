eec26fb5fb77407a0b3002c692c385342f7e45e8
Milashevskyi Bohdan <milasevskijbogdan@gmail.com>
2022-05-20 21:26:36 +0300

    End task

--- To-do_List/To-do_List.cpp
+++ To-do_List/To-do_List.cpp
@@ -23,42 +23,20 @@ struct TodoList
 	TodoItem* todoitems = new TodoItem[size];
 
 
-	void updateItem(TodoItem item)
+	void updateItem(TodoItem item,int chooseItem)
 	{
-		int choose;
-
-		cout << "Choose what u want to change: ";
-			"\n1. id",
-			"\n2. name",
-			"\n3. description",
-			"\n4. day",
-			"\n5. month",
-			"\n6. year",
-			"\n7. deadline";
-		cin >> choose;
-
-		switch (choose)
-		{
-		case 1:
-			cin >> item.id;
-		case 2:
-			cin >> item.name;
-		case 3:
-			cin >> item.description;
-		case 4:
-			cin >> item.day;
-		case 5:
-			cin >> item.month;
-		case 6:
-			cin >> item.year;
-		case 7:
-			cin >> item.deadline;
-		default:
-			cout << "Incorrect input!";
-			break;
-		}
+		todoitems[chooseItem].id = item.id;
+		todoitems[chooseItem].name = item.name;
+		todoitems[chooseItem].description = item.description;
+		todoitems[chooseItem].day = item.day;
+		todoitems[chooseItem].month = item.month;
+		todoitems[chooseItem].year = item.year;
+		todoitems[chooseItem].deadline = item.deadline;
 	}
-	void clean();
+	void clean()
+	{
+		delete todoitems;
+	}
 
 	void addNewItem(TodoItem item)
 	{
@@ -67,7 +45,15 @@ struct TodoList
 			extendArr();
 		}
 		
-		todoitems[counter++] = item;
+		todoitems[counter].id = item.id;
+		todoitems[counter].name = item.name;
+		todoitems[counter].description = item.description;
+		todoitems[counter].day = item.day;
+		todoitems[counter].month = item.month;
+		todoitems[counter].year = item.year;
+		todoitems[counter].deadline = item.deadline;
+
+		counter++;
 
 	}
 
@@ -79,6 +65,7 @@ struct TodoList
 
 		for (int i = 0; i < counter; i++)
 		{
+
 			todoitems[i].id = temp[i].id;
 			todoitems[i].name = temp[i].name;
 			todoitems[i].description = temp[i].description;
@@ -94,15 +81,20 @@ struct TodoList
 	void deleteItem(int id)
 	{
 		TodoItem* newTodoitems = new TodoItem[size];
-		for (int i= 0, k = 0; i < size - 1; i++, k++)
+		for (int i= 0, k = 0; i < size - 1; i++)
 		{
 			if (i == id)
 			{
 				continue;
 			}
-			newTodoitems[k] = todoitems[i];
 			
+			newTodoitems[k] = todoitems[i];
+			k++;
 		}
+		delete todoitems;
+		todoitems = newTodoitems;
+		
+		
 	}
 };
 
@@ -112,24 +104,115 @@ int main()
 	TodoItem todoitem;
 	TodoList list;
 
-	cout << "Enter id: ";
-	cin >> todoitem.id;
-	cout << "Enter name: ";
-	cin >> todoitem.name;
-	cout << "Enter description: ";
-	cin >> todoitem.description;
-	cout << "Enter day: ";
-	cin >> todoitem.day;
-	cout << "Enter month: ";
-	cin >> todoitem.month;
-	cout << "Enter year: ";
-	cin >> todoitem.year;
-	cout << "Enter deadline: ";
-	cin >> todoitem.deadline;
+	
+	int chooseMenuItem;
+
+	cout << "Choose action: "
+		"\n1. Add new item"
+		"\n2. delete item"
+		"\n3. Update item"
+		"\n4. Clean list\n";
+	cin >> chooseMenuItem;
+
+	switch (chooseMenuItem)
+	{
+	case 1:
+	{
+		cout << "Enter id: ";
+		cin >> todoitem.id;
+		cout << "Enter name: ";
+		cin >> todoitem.name;
+		cout << "Enter description: ";
+		cin >> todoitem.description;
+		cout << "Enter day: ";
+		cin >> todoitem.day;
+		cout << "Enter month: ";
+		cin >> todoitem.month;
+		cout << "Enter year: ";
+		cin >> todoitem.year;
+		cout << "Enter deadline: ";
+		cin >> todoitem.deadline;
+
+		list.addNewItem(todoitem);
+		break;
+	}
+	case 2:
+	{
+		int chooseId;
+
+		cout << "Enter item id which u want to delete: ";
+		cin >> chooseId;
+		list.deleteItem(chooseId);
+		break;
+	}
+	case 3:
+	{
+		int chooseItem;
+		cout << "Choose item which u want to change: ";
+		cin >> chooseItem;
+
+		int changePos = -1;
+
+		while (changePos != 0)
+		{
+			TodoItem tempitem;
+
+			cout << "Choose what u want to change: ";
+			"\n1. id",
+				"\n2. name",
+				"\n3. description",
+				"\n4. day",
+				"\n5. month",
+				"\n6. year",
+				"\n7. deadline"
+				"\n0.if	 Exit";
+			cin >> changePos;
+
+			switch (changePos)
+			{
+			case 1:
+				cin >> tempitem.id;
+				break;
+			case 2:
+				cin >> tempitem.name;
+				break;
+			case 3:
+				cin >> tempitem.description;
+				break;
+			case 4:
+				cin >> tempitem.day;
+				break;
+			case 5:
+				cin >> tempitem.month;
+				break;
+			case 6:
+				cin >> tempitem.year;
+				break;
+			case 7:
+				cin >> tempitem.deadline;
+				break;
+			case 0:
+				changePos = 0;
+				break;
+			default:
+				cout << "Incorrect input!";
+				break;
+			}
+		}
+
+		list.updateItem(list.todoitems[chooseItem - 1], chooseItem - 1);
+		break;
+	}
+	case 4:
+	{
+		list.clean();
+		break;
+	}
+	}
 
 	
-	list.addNewItem(todoitem);
 	
+
 
 }
 
